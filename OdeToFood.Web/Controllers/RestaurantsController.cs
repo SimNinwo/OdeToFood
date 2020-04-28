3bbd55d0d9380442d468a38a505c3d2610146b44
Simeon Olorunninwo <Simeon.olorunninwo@wemabank.com>
2020-04-28 16:02:43 +0100

    followed POST redirect Action

--- OdeToFood.Web/Controllers/RestaurantsController.cs
+++ OdeToFood.Web/Controllers/RestaurantsController.cs
@@ -45,7 +45,7 @@ namespace OdeToFood.Web.Controllers
             if(ModelState.IsValid)
             {
                 db.Add(restaurant);
-                return View();
+                return RedirectToAction("Details", new { id = restaurant.Id });
             }
 
             return View();
